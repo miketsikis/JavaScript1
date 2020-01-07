669918f023593ceb5d08b37b8ee996283a2003b7
miketsikis <mtsikis@gmail.com>
2020-01-07 18:48:19 +0200

    Update Exercise 1 Remove the comma.js

--- Week1/homework/js-exercises/Exercise 1 Remove the comma.js
+++ Week1/homework/js-exercises/Exercise 1 Remove the comma.js
@@ -1,5 +1,5 @@
 "use strict";
 let myString = "hello,this,is,a,difficult,to,read,sentence";
 console.log("The length of the string is "+myString.length);
-let repl=myString.replace(","," ");
+let repl=myString.replace(/,/g," ");
 console.log(repl.split(",").join(" "));
